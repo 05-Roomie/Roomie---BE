284907c862bdaeae736a7092abb9bcef185c5dd4
dev-Rhea <dev.rhea4444@gmail.com>
2024-07-21 22:42:58 +0900

    :sparkles: cors, dotenv 설정 추가

--- src/main/java/TEAM5/Roomie/RoomieApplication.java
+++ src/main/java/TEAM5/Roomie/RoomieApplication.java
@@ -1,5 +1,6 @@
 package TEAM5.Roomie;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RoomieApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(RoomieApplication.class, args);
 	}
 
