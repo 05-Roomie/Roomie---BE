90e1574e638d41c133a1316eab1438bd84eae629
dev-Rhea <dev.rhea4444@gmail.com>
2024-07-21 21:53:33 +0900

    :tada: Begin a project

---
+++ src/test/java/TEAM5/Roomie/RoomieApplicationTests.java
@@ -0,0 +1,13 @@
+package TEAM5.Roomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
