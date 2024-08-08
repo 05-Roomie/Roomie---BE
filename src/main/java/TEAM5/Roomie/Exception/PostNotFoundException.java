5a0dfbadbb72701d6d0cdeebd17b5be701a94f9d
rhea <dev.rhea4444@gmail.com>
2024-08-08 09:22:37 +0900

    :sparkles: error 설정

---
+++ src/main/java/TEAM5/Roomie/Exception/PostNotFoundException.java
@@ -0,0 +1,8 @@
+package TEAM5.Roomie.Exception;
+
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+}
+
