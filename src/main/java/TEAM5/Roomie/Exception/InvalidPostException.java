ed4b6c84da2be65fc5765b667a7e5354660c9ee6
rhea <dev.rhea4444@gmail.com>
2024-08-08 09:18:41 +0900

    :sparkles: dotenv 관련 설정 삭제

---
+++ src/main/java/TEAM5/Roomie/Exception/InvalidPostException.java
@@ -0,0 +1,7 @@
+package TEAM5.Roomie.Exception;
+
+public class InvalidPostException extends RuntimeException {
+    public InvalidPostException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/TEAM5/Roomie/Exception/ParticipantException.java
@@ -0,0 +1,7 @@
+package TEAM5.Roomie.Exception;
+
+public class ParticipantException extends RuntimeException {
+    public ParticipantException(String message) {
+        super(message);
+    }
+}
